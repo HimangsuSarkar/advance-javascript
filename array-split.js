e2a7e6355768169d2fab634ae6db1aa2e10d2c78
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-02-03 01:09:57 +0800

    more sloving problem

---
+++ array-split.js
@@ -0,0 +1,6 @@
+const nums = [1, 4, 5, 3, 6, 7];
+//const part = nums.slice(2, 5);
+const remove = nums.splice(2, 2);
+//console.log(remove);
+const together = nums.join(",");
+console.log(together);
